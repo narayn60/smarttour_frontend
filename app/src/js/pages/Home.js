1fbd0a1c33a07bacc2e88edb057e129d6b484887
Andrew Stuart <narayn60@gmail.com>
2016-03-04 15:26:52 +0000

    Blurred frontpage images

--- app/src/js/pages/Home.js
+++ app/src/js/pages/Home.js
@@ -5,24 +5,31 @@ import { Button } from 'react-bootstrap';
 export default class Home extends React.Component {
   render() {
     return (
+
       <div>
-        <header class="header_1">
-          <div class="container">
-            <div class="intro-text">
-              <div class="intro-lead-in">Jaffna Tours</div>
-              <Button class="page-scroll btn btn-xl">Take a tour</Button>
-            </div>
-          </div>
-        </header>
-        <header class="header_2">
-          <div class="container">
-            <div class="right-text">
-              <div class="intro-lead-in">
-                Bringing the best tours available to you
+        <div class="backgroundImageContainer">
+          <div class="background-image header_1"></div>
+          <header>
+            <div class="content container">
+              <div class="intro-text">
+                <div class="intro-lead-in">Jaffna Tours</div>
+                <Button class="page-scroll btn btn-xl">Take a tour</Button>
               </div>
             </div>
-          </div>
-        </header>
+          </header>
+        </div>
+        <div class="backgroundImageContainer">
+          <div class="background-image header_2"></div>
+          <header>
+            <div class="content container">
+              <div class="right-text">
+                <div class="intro-lead-in">
+                  Bringing the best tours available to you
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
       </div>
     );
   }
