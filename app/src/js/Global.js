57c884897b84ba60a3c430188fd279d51d061cb5
George Nash <georgenash@Georges-MBP.default>
2016-03-24 10:14:58 +0000

    tiny alias fix

--- app/src/js/Global.js
+++ app/src/js/Global.js
@@ -1,7 +1,7 @@
 // Store global constants
 class Global {
   constructor() {
-    this.backend_url = 'https://192.168.99.101/auth/';
+    this.backend_url = 'https://192.168.99.100/auth/';
     // this.backend_url = 'https://tourbackend.jaffnalab.com/auth/';
   }
 }
--- app/webpack_conf/resolve_alias.js
+++ app/webpack_conf/resolve_alias.js
@@ -9,10 +9,12 @@ module.exports = {
   GuideStore: store_base + 'GuideStore',
   TourStore: store_base + 'TourStore',
   UserTourStore: store_base + 'UserTourStore',
+  PersonalTourStore: store_base + 'PersonalTourStore',
   MapStore: store_base + 'MapStore',
   AuthActions: action_base + "AuthActions",
   TourActions: action_base + "TourActions",
   UserTourActions: action_base + "UserTourActions",
+  PersonalTourActions: action_base + "PersonalTourActions",
   MapActions: action_base + "MapActions",
   GuideActions: action_base + "GuideActions",
   FormActions: action_base + "FormActions",
@@ -20,6 +22,7 @@ module.exports = {
   GuideSource: source_base + "GuideSource",
   TourSource: source_base + "TourSource",
   UserTourSource: source_base + "UserTourSource",
+  PersonalTourSource: source_base + "PersonalTourSource",
   alt_base: js_base + "alt",
   base_css: "src/css/base.scss"
 }
