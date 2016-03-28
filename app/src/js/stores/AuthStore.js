047df77fb31e0a5437e1b728b3cd16ef89e3df9c
Andrew Stuart <narayn60@gmail.com>
2016-03-28 18:43:08 +0100

    Tidied up directories

--- app/src/js/stores/AuthStore.js
+++ app/src/js/stores/AuthStore.js
@@ -5,25 +5,25 @@ class AuthStore {
   constructor() {
     this.bindActions(AuthActions);
 
-    this.user= {
-      id: null,
-      id_token: null,
-      token: null,
-      name: null,
-      logo: null,
-      provider: null,
-      email: null
-    };
-
     // this.user= {
-    //   id: 1,
-    // id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImFiMzBlMzVmODNkYjljY2I1YjE4YTJkYmMzMDIwM2Q1M2FlN2EwODEifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiM2h4aloxVEtWQkRnR1Q0UHU1MnFRUSIsImF1ZCI6IjM5NzgyMTI1NDE4OS1taHR2azZnbDZrbW1xZWV2bmhzazJuMXMyazd2N24zdC5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNTYzMjM4NDA5MjY5OTg3ODAxMyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiIzOTc4MjEyNTQxODktbWh0dms2Z2w2a21tcWVldm5oc2sybjFzMms3djduM3QuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6Im5hcmF5bjYwQGdtYWlsLmNvbSIsImlhdCI6MTQ1OTEwOTc3MiwiZXhwIjoxNDU5MTEzMzcyLCJuYW1lIjoiQW5kcmV3IFN0dWFydCIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLWtacTd6NTRLWDcwL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUZrL09FeUFkWi1lMTRFL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJBbmRyZXciLCJmYW1pbHlfbmFtZSI6IlN0dWFydCIsImxvY2FsZSI6ImVuLUdCIn0.O00q77kXu3jgMj-TZ8QDTTnhVanz6lCPTNbuH0fkkCnttsF2zcuK8DYA21dNE3N9-Vi4CzzL_NblV6Pd8vwUCVvgOlBiq1RhFoF7CqkUXUQV-XbL9L72_8RRq4aG2Zs_OuPz4vF-2-gUtlFcR3cNrBrXDh0iyIyITxI7WXx6ycxUU6KgBN6X4z8Dqr8dMGtB7Lq_vLoz2zAiZ6sELcB_HuPD0VHptEMot5a-kn_jAjHK-itGxOZPV8vt4gF6LtRbIMrZbmxHHxzfKpVCGUwZhjBJRWp1a3fshoGiPoSGsegzJaMR6wmownKWiN-v5nMERKjqHr8cJ4jtsqOl_bq45g',
-    //   token: 1,
-    //   name: 'Andrew Stuart',
+    //   id: null,
+    //   id_token: null,
+    //   token: null,
+    //   name: null,
     //   logo: null,
-    //   provider: 'google',
-    //   email: 'narayn60@gmail.com'
+    //   provider: null,
+    //   email: null
     // };
+
+    this.user= {
+      id: 1,
+    id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImFiMzBlMzVmODNkYjljY2I1YjE4YTJkYmMzMDIwM2Q1M2FlN2EwODEifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiM2h4aloxVEtWQkRnR1Q0UHU1MnFRUSIsImF1ZCI6IjM5NzgyMTI1NDE4OS1taHR2azZnbDZrbW1xZWV2bmhzazJuMXMyazd2N24zdC5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNTYzMjM4NDA5MjY5OTg3ODAxMyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiIzOTc4MjEyNTQxODktbWh0dms2Z2w2a21tcWVldm5oc2sybjFzMms3djduM3QuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6Im5hcmF5bjYwQGdtYWlsLmNvbSIsImlhdCI6MTQ1OTEwOTc3MiwiZXhwIjoxNDU5MTEzMzcyLCJuYW1lIjoiQW5kcmV3IFN0dWFydCIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLWtacTd6NTRLWDcwL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUZrL09FeUFkWi1lMTRFL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJBbmRyZXciLCJmYW1pbHlfbmFtZSI6IlN0dWFydCIsImxvY2FsZSI6ImVuLUdCIn0.O00q77kXu3jgMj-TZ8QDTTnhVanz6lCPTNbuH0fkkCnttsF2zcuK8DYA21dNE3N9-Vi4CzzL_NblV6Pd8vwUCVvgOlBiq1RhFoF7CqkUXUQV-XbL9L72_8RRq4aG2Zs_OuPz4vF-2-gUtlFcR3cNrBrXDh0iyIyITxI7WXx6ycxUU6KgBN6X4z8Dqr8dMGtB7Lq_vLoz2zAiZ6sELcB_HuPD0VHptEMot5a-kn_jAjHK-itGxOZPV8vt4gF6LtRbIMrZbmxHHxzfKpVCGUwZhjBJRWp1a3fshoGiPoSGsegzJaMR6wmownKWiN-v5nMERKjqHr8cJ4jtsqOl_bq45g',
+      token: 1,
+      name: 'Andrew Stuart',
+      logo: null,
+      provider: 'google',
+      email: 'narayn60@gmail.com'
+    };
 
     this.exportPublicMethods({
       isLoggedIn: this.isLoggedIn,
