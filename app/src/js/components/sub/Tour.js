ff26a54b455db8baacf9ceb27abbdd837e241eef
George Nash <georgenash@Georges-MBP.default>
2016-04-05 15:41:31 +0100

    Popout to download qr on browse tours

--- app/src/js/components/sub/Tour.js
+++ app/src/js/components/sub/Tour.js
@@ -2,51 +2,90 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Row, Col, Button, Well, Collapse } from "react-bootstrap";
 import { Router, Route, Link, browserHistory } from 'react-router';
+import Tooltip from 'rc-tooltip';
+import ImageLoad from './ImageLoad';
+import 'rc-tooltip/assets/bootstrap.css';
 
 export default class Tour extends React.Component {
 
-  render() {
-    const { name, owner, id, guide } = this.props.tour;
+    constructor() {
+        super();
+        this.state = {'visible': false}
+    }
 
-    return (
-        <div class="col-md-6 col-sm-6 portfolio-item">
-            <div class="portfolio-heading">
-                <h4>{ name }</h4>
-            </div>
-            <Link to={`/browse/tours/${id}`} class="portfolio-link"> 
-                    <div class="portfolio-hover">
-                        <div class="portfolio-hover-content">
-                            <Row>
-                                <Col md={6} class="tour-hover-content">
-                                    <i class="fa fa-plus fa-2x"></i>
-                                    <p> Explore </p>
-                                </Col>
-                                <Col md={6} class="tour-hover-content">
-                                    <i class="fa fa-arrow-down fa-2x"></i>
-                                    <p> Download </p>
-                                </Col>
-                            </Row>
-                        </div>
+    handleVisibleChange(visible) {
+        this.setState({
+            visible: visible,
+            });
+    }
+
+    handleDestroy() {
+        this.setState({
+            destroy:true,
+        });
+    }
+
+    preventDefault(e) {
+        e.preventDefault();
+    }
+
+    // Link to={`/browse/tours/${id}`} 
+
+    render() {
+        const { name, owner, id, guide } = this.props.tour;
+        const qr_path = "/media/tours/" + id + "/qrcode_profile/"
+
+            return (
+                <div class="col-md-6 col-sm-6 portfolio-item">
+                    <div class="portfolio-heading">
+                        <h4>{ name }</h4>
                     </div>
-                    <img src="img/portfolio/roundicons.png" class="img-responsive" alt=""/>
-            </Link>
-            <div class="portfolio-caption">
-                <Row>
-                    <Col md={4} >
-                        <i class="fa fa-user"></i>
-                        <p>{ guide.full_name }</p>
-                    </Col>
-                    <Col md={4} >
-                        <i class="fa fa-at"></i>
-                        <p>{ guide.email }</p>
-                    </Col>
-                    <Col md={4}>
-                        <i class="fa fa-heart"></i>
-                        <p>{ this.props.tour.followers }</p>
-                    </Col>
-                </Row>
-            </div>
-        </div>
-    );
-  }
-}
+                    <div class="portfolio-link"> 
+                            <div class="portfolio-hover">
+                                <div class="portfolio-hover-content">
+                                    <Row>
+                                        <Col md={6} class="tour-hover-content">
+                                            <i class="fa fa-plus fa-2x"></i>
+                                            <p> Explore </p>
+                                        </Col>
+                                        <Col md={6} class="tour-hover-content">
+                                            <Tooltip
+                                              visible={this.state.visible}
+                                              animation="zoom"
+                                              onVisibleChange={this.handleVisibleChange.bind(this)}
+                                              trigger="click"
+                                              overlay= {
+                                                    <div>
+                                                        <span>Scan</span>
+                                                        <ImageLoad path={qr_path} />
+                                                    </div>
+                                                }>
+                                              <a class="fa fa-arrow-down fa-2x"></a>
+                                            </Tooltip>
+                                            <p> Download </p>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </div>
+                            <img src="img/portfolio/roundicons.png" class="img-responsive" alt=""/>
+                    </div>
+                    <div class="portfolio-caption">
+                        <Row>
+                            <Col md={4} >
+                                <i class="fa fa-user"></i>
+                                <p>{ guide.full_name }</p>
+                            </Col>
+                            <Col md={4} >
+                                <i class="fa fa-at"></i>
+                                <p>{ guide.email }</p>
+                            </Col>
+                            <Col md={4}>
+                                <i class="fa fa-heart"></i>
+                                <p>{ this.props.tour.followers }</p>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            );
+            }
+            }
