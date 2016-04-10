a43ddee3e3bc00041ea9d4cf79c6f7ecb30de0ea
Andrew Stuart <narayn60@gmail.com>
2016-04-10 08:58:59 +0100

    Placeholders

--- app/src/js/components/containers/PersonalTourOverviewContainer.js
+++ app/src/js/components/containers/PersonalTourOverviewContainer.js
@@ -6,45 +6,51 @@ import LocationTable from '../sub/LocationTable';
 
 export default class PersonalTourOverviewContainer extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: 0
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 0
+    };
+  }
+
+  __handleClick(index) {
+    this.setState({selected: index});
+  }
+
+  render() {
+
+    console.log("PersonalTourOverview", this.props.locations, this.state.selected);
+
+    if (this.props.locations.length === 0) {
+      return (
+        <div>
+          No Locations Created
+        </div>
+      );
     }
 
-    __handleClick(index) {
-        this.setState({selected: index});
-    }
-
-    render() {
-
-        console.log("PersonalTourOverview", this.props.locations, this.state.selected);
-
-        if (this.props.locations.length === 0) {
-            return (
-                    <div>
-                    No Locations Created
-                    </div>
-                   )
-        }
-
-        return (
-            <Grid fluid={true}>
-                <Row style={{height: '400px'}}>
-                    <Col md={6} style={{height: '100%'}}>
-                        <TourMap
-                            handleClick={this.__handleClick.bind(this)}
-                            locations={this.props.locations}
-                            selected={this.state.selected}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6}>
-                        <LocationTable locations={this.props.locations} onClick={this.__handleClick.bind(this)}/>
-                    </Col>
-                </Row>
-            </Grid>
-           )
-    }
+    return (
+      <Grid fluid={true}>
+        <Row style={{height: '400px'}}>
+          <Col md={6} style={{height: '100%'}}>
+            <TourMap
+              handleClick={this.__handleClick.bind(this)}
+              locations={this.props.locations}
+              selected={this.state.selected}/>
+          </Col>
+          <Col md={6}>
+            Placholder for side, will hold tour overview
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <LocationTable locations={this.props.locations} onClick={this.__handleClick.bind(this)}/>
+          </Col>
+          <Col md={6}>
+            Will hold review list
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
