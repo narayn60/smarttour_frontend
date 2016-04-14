5bc9974bf3cd4975bf70e6fa581af06644239305
Andrew Stuart <narayn60@gmail.com>
2016-04-14 14:34:26 +0100

    Changes

--- app/src/js/components/sub/TourFormSuccess.js
+++ app/src/js/components/sub/TourFormSuccess.js
@@ -10,23 +10,21 @@ export default class TourFormSuccess extends React.Component {
   }
 
   render() {
-    var qr_path = "/media/tours/" + this.props.newTourID + "/qrcode_grid/"
+    var qr_path = "/media/tours/" + this.props.newTourID + "/qrcode_grid/";
 
     return (
       <div>
-        <Col md={4} mdOffset={4}>
-          <h2>Successfully Created {this.props.fieldValues.name}!</h2>
-          <Row class="text-center">
-            <Button bsStyle="success" bsSize="large" class="tourButton text-center" onClick={ ()=> this.setState({ open: !this.state.open })}>
-              Download your QR codes
-            </Button>
-            <Collapse in={this.state.open}>
-              <div>
-                  <ImageLoad path={qr_path} />
-              </div>
-            </Collapse>
-          </Row>
-        </Col>
+        <h2>Successfully Created {this.props.fieldValues.name}!</h2>
+        <Row class="text-center">
+          <Button bsStyle="success" bsSize="large" class="tourButton text-center" onClick={ ()=> this.setState({ open: !this.state.open })}>
+            Download your QR codes
+          </Button>
+          <Collapse in={this.state.open}>
+            <div>
+              <ImageLoad path={qr_path} />
+            </div>
+          </Collapse>
+        </Row>
       </div>
     );
   }
