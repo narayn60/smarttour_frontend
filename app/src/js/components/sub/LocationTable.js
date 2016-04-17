b88323403f3496aeb5ab65943ee1de953271e1bc
Andrew Stuart <narayn60@gmail.com>
2016-04-17 16:45:37 +0100

    Fixed location table issue

--- app/src/js/components/sub/LocationTable.js
+++ app/src/js/components/sub/LocationTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Well, Button, Collapse, Image, Table } from "react-bootstrap";
+import { Grid, Row, Col, Panel, Button, Image } from "react-bootstrap";
 import AuthStore from 'AuthStore';
 import Global from 'Global';
 
@@ -27,44 +27,40 @@ export default class LocationTable extends React.Component {
   render() {
 
     const locations = this.props.locations;
-	  const path = Global.backend_url + AuthStore.getUid() + "/";
+    const path = Global.backend_url + AuthStore.getUid() + "/";
 
-    console.log("Open", this.state.open);
+    console.log("Location", locations);
 
     var locationTable = locations.map((location, i) =>
-      [
-      <tr onClick={() => this.props.onClick(i)}>
-        <td style={{height: '100px', width: '100px'}}>
-          <Image src={path + location.qrcode_path_s3} class="img-responsive" style={{objectFit: 'contain'}}/>
-        </td>
-        <td class="location-data">
-          <h5>{location.name}</h5>
-          <p>
-            <i class="fa fa-map-marker"></i>
-            { location.address }
-          </p>
-          <Button onClick={() => this.__toggleCollapse(i)}>
-            Extra Info
-          </Button>
-        </td>
-      </tr>,
-        <Collapse in={this.state.open.has(i)}>
-          <div>
-            <Well>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-            </Well>
-          </div>
-        </Collapse>
-      ]
+      (
+        <div onClick={() => this.props.onClick(i)}>
+          <Row onClick={() => this.__toggleCollapse(i)}>
+            <Col md={4} style={{height: '100px', width: '100px'}}>
+              <Image src={path + location.qrcode_path_s3} class="img-responsive" style={{objectFit: 'contain'}}/>
+            </Col>
+            <Col md={8} class="location-data">
+              <h5>{location.name}</h5>
+              <p>
+                <i class="fa fa-map-marker"></i>
+                { location.address }
+              </p>
+            </Col>
+          </Row>
+          <Panel collapsible expanded={this.state.open.has(i)}>
+            <Grid>
+              <Row>
+                About: { location.about}
+              </Row>
+            </Grid>
+          </Panel>
+        </div>
+      )
     );
 
     return (
-      <Table bordered condense hover class="tableSection">
-        <tbody>
-          { locationTable }
-        </tbody>
-      </Table>
+      <div>
+        {locationTable}
+      </div>
     );
   }
 }
