43e039e801ca275556589c8f68a93d4f097f89fc
Andrew Stuart <narayn60@gmail.com>
2016-04-25 20:27:07 +0100

    Improved create tour page

--- app/src/js/Global.js
+++ app/src/js/Global.js
@@ -1,8 +1,8 @@
 // Store global constants
 class Global {
   constructor() {
-    this.backend_url = 'https://192.168.99.100/auth/';
-    // this.backend_url = 'https://tourbackend.jaffnalab.com/auth/';
+    // this.backend_url = 'https://192.168.99.100/auth/';
+    this.backend_url = 'https://tourbackend.jaffnalab.com/auth/';
   }
 }
 
--- app/src/js/stores/AuthStore.js
+++ app/src/js/stores/AuthStore.js
@@ -5,27 +5,27 @@ class AuthStore {
   constructor() {
     this.bindActions(AuthActions);
 
-    // this.user= {
-    //   id: null,
-    //   id_token: null,
-    //   refresh_token: null,
-    //   token: null,
-    //   name: null,
-    //   logo: null,
-    //   provider: null,
-    //   email: null
-    // };
-
     this.user= {
-      id: 1,
-      id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjcyYjRkYWIwNDc2MWQ1OWJmY2Q0NTljYmQ2NWEwOGM0YTdiODI1ZDIifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiaGpQTGgwOEI4U2RUemxGZURtOE1BQSIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE2MTA1OTAsImV4cCI6MTQ2MTYxNDE5MCwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.Ks1zwgcCIqf9-t1BTbDAjJJqeYxHPLEYGZIDJqk-onMmByK57nOwrDwUovyMPzFIPE551BxdbYCz46hVc9_eVE26IZK1R9jFlv2Zvz8Gy3qz4dP-BGNR52PrloD-Fit1CrYDeMgDoMaPPSPVzHGFVkWsehUmDnsLAMJ5TwYZqRtXAkzzl_iBHRxSOOTX2doBSi7PAm3-s5AURyjkQLJIrr97xcQ7yI-S4R8Xu0XVa88Jt7_W2QRk8-f_-n9aOGb5I4b98rrjk8OrczOpJ4vY2XbC6s7zu0jpqwG9A6i3Z3wPDz6bz35eMXAr-6Ge70VRIJSYWi--V2-VU8lEbgovkA',
+      id: null,
+      id_token: null,
       refresh_token: null,
-      token: 1,
-      name: 'Test',
+      token: null,
+      name: null,
       logo: null,
-      provider: 'google',
-      email: 'jaffnalab@gmail.com'
+      provider: null,
+      email: null
     };
+
+    // this.user= {
+    //   id: 1,
+    //   id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjcyYjRkYWIwNDc2MWQ1OWJmY2Q0NTljYmQ2NWEwOGM0YTdiODI1ZDIifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiaGpQTGgwOEI4U2RUemxGZURtOE1BQSIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE2MTA1OTAsImV4cCI6MTQ2MTYxNDE5MCwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.Ks1zwgcCIqf9-t1BTbDAjJJqeYxHPLEYGZIDJqk-onMmByK57nOwrDwUovyMPzFIPE551BxdbYCz46hVc9_eVE26IZK1R9jFlv2Zvz8Gy3qz4dP-BGNR52PrloD-Fit1CrYDeMgDoMaPPSPVzHGFVkWsehUmDnsLAMJ5TwYZqRtXAkzzl_iBHRxSOOTX2doBSi7PAm3-s5AURyjkQLJIrr97xcQ7yI-S4R8Xu0XVa88Jt7_W2QRk8-f_-n9aOGb5I4b98rrjk8OrczOpJ4vY2XbC6s7zu0jpqwG9A6i3Z3wPDz6bz35eMXAr-6Ge70VRIJSYWi--V2-VU8lEbgovkA',
+    //   refresh_token: null,
+    //   token: 1,
+    //   name: 'Test',
+    //   logo: null,
+    //   provider: 'google',
+    //   email: 'jaffnalab@gmail.com'
+    // };
 
     this.exportPublicMethods({
       isLoggedIn: this.isLoggedIn,
