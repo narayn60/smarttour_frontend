e83d897fef8105effa4bf73da55a24c7a314320e
Andrew Stuart <narayn60@gmail.com>
2016-04-25 20:26:39 +0100

    Improved create tour page

--- app/src/js/components/sub/TourFormSuccess.js
+++ app/src/js/components/sub/TourFormSuccess.js
@@ -1,30 +1,49 @@
 import React from 'react';
-import { Row, Col, Button, Well, Collapse } from "react-bootstrap";
+import { Image, Row, Col, Button, Well, Modal, Collapse } from "react-bootstrap";
 import ImageLoad from './ImageLoad';
+import AuthStore from 'AuthStore';
+import Global from 'Global';
 
 export default class TourFormSuccess extends React.Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      open: false,
+      showModal: false
+    };
   }
 
   render() {
-    var qr_path = "/media/tours/" + this.props.newTourID + "/qrcode_grid/";
+
+    const qr_path = "/media/tours/" + this.props.newTourID + "/qrcode_grid/";
+    const qr_full_path = Global.backend_url + AuthStore.getUid() + qr_path;
+
+    const success_modal = (
+        <Modal show={this.state.showModal} onHide={() => this.setState({showModal: false})}>
+          <Modal.Header>
+            <Modal.Title>Download QR Codes</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Image class="img-responsive" src={qr_full_path} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button href={qr_full_path} download="Test">Download QR Codes</Button>
+            <Button bsStyle="primary" onClick={() => this.setState({showModal: false})}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+    );
+
 
     return (
       <div>
         <h2>Successfully Created {this.props.fieldValues.name}!</h2>
         <Row class="text-center">
-          <Button bsStyle="success" bsSize="large" class="tourButton text-center" onClick={ ()=> this.setState({ open: !this.state.open })}>
+          <Button bsStyle="success" bsSize="large" class="tourButton text-center" onClick={() => this.setState({ showModal: true })}>
             Download your QR codes
           </Button>
-          <Collapse in={this.state.open}>
-            <div>
-              <ImageLoad path={qr_path} />
-            </div>
-          </Collapse>
         </Row>
+        {success_modal}
       </div>
     );
   }
