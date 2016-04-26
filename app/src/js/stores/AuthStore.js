2a3d5827ca58e786fd87c282211678d6ad41253e
Andrew Stuart <narayn60@gmail.com>
2016-04-26 16:58:38 +0100

    Ability to view note and relevant photo

--- app/src/js/stores/AuthStore.js
+++ app/src/js/stores/AuthStore.js
@@ -5,27 +5,27 @@ class AuthStore {
   constructor() {
     this.bindActions(AuthActions);
 
-    // this.user= {
-    //   id: null,
-    //   id_token: null,
-    //   refresh_token: null,
-    //   token: null,
-    //   name: null,
-    //   logo: null,
-    //   provider: null,
-    //   email: null
-    // };
-
     this.user= {
-      id: 1,
-      id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjUwMjlhYmQ4ZDM1MDAzYjFiNDlmNTZhYmZiNTc3ZmNiNWQ1NGMxNjcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiUUFGN2h0VTBydTN4WkE1RUMzLTlqUSIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE2Nzk1MjgsImV4cCI6MTQ2MTY4MzEyOCwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.PgwWJ0mQUgxdmNl4LbAwg9sGC9_yLaV0dkf9pcqfXao0SbRUUTj1pICZ3jZh-Pv5v6wqdlbpxsz-jIvUCb2tqJ_KOnyHSgqo6bdiwON3eHK_zECdaBVnMXACK6AxM-uAASRdCn6rmH3kQq-mtPp1J782legW-B-w4KMtp1MO8vPoM0JXELd5H0nlfqC8Nq4s-hw1JcIWCAaJtqI6Ifm2jovoYXRg4rXuAQfc1-i8nSZ-dxFUOijrouOlkXdcN-MPbz-Og_6w29GXya2uIVmx61FzrZ6JFX-ypxNr9_3qbbrL6I-wpnglUpFMDYUgAdpl_e9jZdDMGPhGObd6X7LMPg',
+      id: null,
+      id_token: null,
       refresh_token: null,
-      token: 1,
-      name: 'Test',
+      token: null,
+      name: null,
       logo: null,
-      provider: 'google',
-      email: 'jaffnalab@gmail.com'
+      provider: null,
+      email: null
     };
+
+    // this.user= {
+    //   id: 1,
+    //   id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjUwMjlhYmQ4ZDM1MDAzYjFiNDlmNTZhYmZiNTc3ZmNiNWQ1NGMxNjcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiYTA1QjI3VWUtd1BFZVZLSVJ1amo5ZyIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE2ODUzNzEsImV4cCI6MTQ2MTY4ODk3MSwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.fWHGrp3iDHn9KNmQGFdzaMgBZU1mWM3sCr1ifG6Nbvl7LlcuH-CZcW4TjGj9SYHNufERaS_-36rAsVRgPbl8qZ2rvetybYI3rWtm1SbzuYXeE5ywy0bh-VL3rl1q_7PuHd-CC2reQP9xTknvIGDe7o2QLOD3Jj7CQU8p2wKRvQJ3w6H8UG4VBgf2yE6lXfx2fDS4HunogSlrU-bnjK9cwAw3EvJondX88gkou3LE1x_rrCndwXOMRklNV9nlXOvpnAqOB4-WQEPHhW344GPaJfWz8x0vzL0VMM-SXG4BY-V93n7pt4eDmQ2kdrHbvpR2Fwj-Jef85amu7ODwElSP-A',
+    //   refresh_token: null,
+    //   token: 1,
+    //   name: 'Test',
+    //   logo: null,
+    //   provider: 'google',
+    //   email: 'jaffnalab@gmail.com'
+    // };
 
     this.exportPublicMethods({
       isLoggedIn: this.isLoggedIn,
