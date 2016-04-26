ef58f256b01786356d5a669ff21590a90576abbb
Andrew Stuart <narayn60@gmail.com>
2016-04-26 14:50:15 +0100

    Delete tour button placeholder

--- app/src/js/components/sub/TourTable.js
+++ app/src/js/components/sub/TourTable.js
@@ -4,24 +4,44 @@ import { browserHistory } from 'react-router';
 import { Button } from 'react-bootstrap';
 import TableImage from './TableImage';
 
+class DeleteButton extends React.Component {
 
+  __handleBtnClick(e) {
+    e.stopPropagation();
+    console.log("Need to delete", this.props.tour_id);
+  }
+
+  render() {
+    return (
+      <Button onClick={this.__handleBtnClick.bind(this)}>
+        <i class="fa fa-times" style={{color: "red"}}></i>
+      </Button>
+
+    );
+  }
+}
 export default class TourTable extends React.Component {
 
   constructor(props) {
     super(props);
   }
 
-  imageFormatter(cell, row) {
+  __imageFormatter(cell, row) {
     return <TableImage image_url={cell}/>;
   }
+
+  activeFormatter(cell, row) {
+    return (
+      <DeleteButton tour_id={row.id}/>
+    );
+  }
+
 
   rowClick(row) {
     browserHistory.push('/mytours/' + row.id);
   }
 
   render() {
-
-    console.log("Tours", this.props.tours);
 
     //TODO: Instead of saying no tours, add button to create tour
     if (this.props.tours.length === 0) {
@@ -44,10 +64,11 @@ export default class TourTable extends React.Component {
 
     return (
         <BootstrapTable data={this.props.tours} options={options} hover={true} pagination={true} condensed={true} search={true} striped={false} bordered={false}>
-        <TableHeaderColumn dataField="img_url" width="120px" dataFormat={this.imageFormatter} dataAlign="center">Icon</TableHeaderColumn>
+        <TableHeaderColumn dataField="img_url" width="120px" dataFormat={this.__imageFormatter} dataAlign="center">Icon</TableHeaderColumn>
         <TableHeaderColumn dataField="name" isKey={true} dataAlign="center" dataSort={true}>Tour Name</TableHeaderColumn>
         <TableHeaderColumn dataField="followers" dataAlign="center" dataSort={true}>Followers</TableHeaderColumn>
         <TableHeaderColumn dataField="points" dataAlign="center" dataSort={true}>Locations Specified</TableHeaderColumn>
+        <TableHeaderColumn dataField="delete" dataAlign="center" dataFormat={this.activeFormatter}>Delete Tour</TableHeaderColumn>
       </BootstrapTable>
 
     );
--- app/src/js/stores/AuthStore.js
+++ app/src/js/stores/AuthStore.js
@@ -5,27 +5,27 @@ class AuthStore {
   constructor() {
     this.bindActions(AuthActions);
 
-    this.user= {
-      id: null,
-      id_token: null,
-      refresh_token: null,
-      token: null,
-      name: null,
-      logo: null,
-      provider: null,
-      email: null
-    };
-
     // this.user= {
-    //   id: 1,
-    //   id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjUwMjlhYmQ4ZDM1MDAzYjFiNDlmNTZhYmZiNTc3ZmNiNWQ1NGMxNjcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoia082S2dwaVZSQUtGeV9zVFozM18wUSIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE2NzIzOTAsImV4cCI6MTQ2MTY3NTk5MCwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.ky5QkVGsLgbwlEjNT8ZucfyUyefSy8C8VIM3N3mfQ50W-MYJxr0FQKAGlW9QB6xLiRfS8lIq2LWBvsfla_jcEIl48_Hs1JitB3m7a9b1t2ee7B_bR1Q_GeeGZedXzbNLh1F-BT5v3IfKRNuupzFJFNtL5BN0nSxOO6oSZK6lZt-5iGjNx1GH_pSAOALjF1DA0up2ugrgePYkzioEfhpmW6qs--AQklGUbE_7dvkAuD54N_Kr5SFxmpjfuAF2nCErRbSBn4cSJ91PIpyl69uXgUyGUxYZJcTMVnBRf4PcAvbvfBZESQW4AMBJtw-Z-wppcfOv8xUeeFRyoCRJxACH4g',
+    //   id: null,
+    //   id_token: null,
     //   refresh_token: null,
-    //   token: 1,
-    //   name: 'Test',
+    //   token: null,
+    //   name: null,
     //   logo: null,
-    //   provider: 'google',
-    //   email: 'jaffnalab@gmail.com'
+    //   provider: null,
+    //   email: null
     // };
+
+    this.user= {
+      id: 1,
+      id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjUwMjlhYmQ4ZDM1MDAzYjFiNDlmNTZhYmZiNTc3ZmNiNWQ1NGMxNjcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiOFdGakpDclBzRGV0THZyMU55bWVsQSIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE2NzYzOTQsImV4cCI6MTQ2MTY3OTk5NCwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.HHmtw2kmmzMVV_O1gIW_Xc_nmKa1oCuaYCdgX5U-YfcV-UipwaQ0sJSxcXVFDd93y8Ip-RGy1OSonatUOAWPytH3dUHum901MTUDo7_srfbiBmW2K7NNNoU4hAJQUhsKIQ6cJMnsN8K37YJahyMMf91lX6HLKl01JA1g928CfMJW54I1rh_d1W-J7nqONtfR4CPKcUPm_cbbCGd4fL46MUzrCZ1xiJwPUJsmFBaEym2ChXfqeH39vi7mWLN4TCtdyWMTTarSoSe-xP1CnsUjr4PJ2NOnENheIJhbBUgNAuPBpYuI_LkAgPVg1TS5tJFObylx5eGa8lgLdmcS0we6pw',
+      refresh_token: null,
+      token: 1,
+      name: 'Test',
+      logo: null,
+      provider: 'google',
+      email: 'jaffnalab@gmail.com'
+    };
 
     this.exportPublicMethods({
       isLoggedIn: this.isLoggedIn,
