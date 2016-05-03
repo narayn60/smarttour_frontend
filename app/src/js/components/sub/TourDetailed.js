5d445666faaef506e5b51d14d60b755725693ed8
George Nash <georgenash@Georges-MBP.home>
2016-05-03 20:33:26 +0100

    can't review if you're the owner

--- app/src/js/components/sub/TourDetailed.js
+++ app/src/js/components/sub/TourDetailed.js
@@ -125,7 +125,7 @@ export default class TourDetailed extends React.Component {
       </div>
     );
 
-    const sections = [location_table, "Followers", <ReviewList reviews={this.state.tour.reviews} tour_id={this.props.tour_id} personal={false}/>];
+    const sections = [location_table, "Followers", <ReviewList reviews={this.state.tour.reviews} tour_id={this.props.tour_id} personal={false} guide={this.state.tour.guide}/>];
 
     return (
       <Grid>
--- app/src/js/stores/AuthStore.js
+++ app/src/js/stores/AuthStore.js
@@ -5,34 +5,35 @@ class AuthStore {
   constructor() {
     this.bindActions(AuthActions);
 
-    this.user= {
-      id: null,
-      id_token: null,
-      refresh_token: null,
-      token: null,
-      name: null,
-      logo: null,
-      provider: null,
-      email: null
-    };
-
     // this.user= {
-    //   id: 1,
-    //   id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjZhZjhkM2U0ZThkYzhkNWY4MjhmNzI0NjY0OWJkZmNiZTI0NTZiNGIifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiYWtYY2VXVGFJd2FILW1TeGVldmFDZyIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjE5MzkwODUsImV4cCI6MTQ2MTk0MjY4NSwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.Q6UKdLowh26rBo0-Nhgz9De_2sw1zVQTsB0aE_rQ64QQaNU-Oq2ca3MSLxu6wUgGVhd1ef-Wq8CZMQEBrXXAcLsXQYQ5MWqmObNdiB_GhmCOpSuMIoABBEwkDOpDwfTAy7a2DqSfiB7vV34m66MfSioKt4EKoYiJ6jBUyybfncQB4VimCNkmoc7PTe-QxBUR1x2HJysbzlP7jD0aQhwHgb4Gupgamt4HWE5cl8U7cIx95uO4-9GmMypQAlu2Jn48t0PRTeQZuznDrIOZNULWqfGEKd1kC_LCywjOrCD2Py2MEEhEJ2ppi4CBV_TYrDi6wh4IlwfIvGSDfhunqoy2Lw',
+    //   id: null,
+    //   id_token: null,
     //   refresh_token: null,
-    //   token: 1,
-    //   name: 'Test',
+    //   token: null,
+    //   name: null,
     //   logo: null,
-    //   provider: 'google',
-    //   email: 'jaffnalab@gmail.com'
+    //   provider: null,
+    //   email: null
     // };
+
+    this.user= {
+      id: 1,
+      id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImFjM2RmYjM3ZDhhODA5NTI0Mzk3Yjk1M2I1NGU1ODZhMzY3NDU2NzcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiZl9qYmZLeExQWDE4ckZVakJMN2dnZyIsImF1ZCI6Ijk4MTU2NzMyMzU5Ni1sNHNwaTMyM2J1ajl0MTl2dDh2ZmZtdWRnaWluZDdoai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNDAxMzEzNTk2MDQ5ODgwNDgxNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI5ODE1NjczMjM1OTYtbDRzcGkzMjNidWo5dDE5dnQ4dmZmbXVkZ2lpbmQ3aGouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImphZmZuYWxhYkBnbWFpbC5jb20iLCJpYXQiOjE0NjIzMDI5NDksImV4cCI6MTQ2MjMwNjU0OSwibmFtZSI6IkphZmZuYSBUZWFtIiwiZ2l2ZW5fbmFtZSI6IkphZmZuYSIsImZhbWlseV9uYW1lIjoiVGVhbSIsImxvY2FsZSI6ImVuIn0.e19MfhekPTDcQ95Z2fexXTMlph6HXiFRpgPhfOen3wYkhiAXRl6KNY-JGoTFydbbh_-1Mdk7pEtaAafXjMk1nrxs2xZhffRdaNdWyccv0u_P23Irrf27FBdlZt7I1m4x3N3tt6Wb_10N4QyYHYW9C1iqVUvPlGxkbgXv7D1iCep1YlPpn8BHjeGcDgSeE6r2hLkLHTogRK6TjSu6nFcrGJpNTLj0DB-enTMOgfyVMgO5nbSr-7gyx740rbP4cJugwAW4gYbjkuL-SxHcx4i2SEO02Sv2pGTNjnW5eBBR4XfhI8gLWUcB6Uj6meh7-o3I9Rpj699X1na9XEOzSmm-MQ',
+      refresh_token: null,
+      token: 1,
+      name: 'Test',
+      logo: null,
+      provider: 'google',
+      email: 'jaffnalab@gmail.com'
+    };
 
     this.exportPublicMethods({
       isLoggedIn: this.isLoggedIn,
       getUser: this.getUser,
       getUid: this.getUid,
       getName: this.getName,
-      getEmail: this.getEmail
+      getEmail: this.getEmail,
+      getId: this.getId,
     });
   }
 
@@ -57,6 +58,11 @@ class AuthStore {
   getUid() {
     const st = this.getState().user;
     return st.id_token;
+  }
+
+  getId() {
+    const st = this.getState().user;
+    return st.id;
   }
 
   getName() {
