da1c9720c173ab5e8794ecdb1b23567dbd66a10f
Andrew Stuart <narayn60@gmail.com>
2016-07-11 19:54:50 +0100

    Included auth.js

---
+++ app/server/config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// };
+
+module.exports = {
+  'googleAuth' : {
+    'clientID' : 'CLIENT_ID',
+    'clientSecret': 'CLIENT_SECRET',
+    // 'callbackURL': 'https://127.0.0.1/auth/google/callback'
+    'callbackURL': 'https://tour.jaffnalab.com/auth/google/callback'
+  }
+};
